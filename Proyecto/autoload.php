b75ced6ae8ffe0c03a94a1cdd2d8152340106f7e
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-21 17:41:56 +0200

    Añadiendo archivo autoload

---
+++ Proyecto/autoload.php
@@ -0,0 +1,10 @@
+<?php
+spl_autoload_register(function ($class) {
+    $paths = ['models/', 'controller/'];
+    foreach ($paths as $path) {
+        $file = __DIR__ . '/' . $path . $class .  '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+});
