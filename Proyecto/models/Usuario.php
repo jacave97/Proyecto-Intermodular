8f529f5b6f20ebdbc1dc4efdcbdc46b15b55643c
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-21 18:42:36 +0200

    Añadiendo Getters y Setters al archivo Usuarios

--- Proyecto/models/Usuario.php
+++ Proyecto/models/Usuario.php
@@ -17,4 +17,94 @@ class Usuario{
         $this->rol=$rol;
       
     }
+
+    /**
+     * Get the value of id_usuario
+     */
+    public function getIdUsuario()
+    {
+        return $this->id_usuario;
+    }
+
+    /**
+     * Set the value of id_usuario
+     */
+    public function setIdUsuario($id_usuario)
+    {
+        $this->id_usuario = $id_usuario;
+
+        
+    }
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        
+    }
+
+    /**
+     * Get the value of rol
+     */
+    public function getRol()
+    {
+        return $this->rol;
+    }
+
+    /**
+     * Set the value of rol
+     */
+    public function setRol($rol)
+    {
+        $this->rol = $rol;
+
+     
+    }
 }
