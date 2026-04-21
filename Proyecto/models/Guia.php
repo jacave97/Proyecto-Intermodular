8f62f1b4ed2fd0e052e181aa5a140de0e14310d1
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-21 17:56:06 +0200

    Añadiendo Getters y Setters al archivo Guia.php

--- Proyecto/models/Guia.php
+++ Proyecto/models/Guia.php
@@ -14,5 +14,93 @@ class Guia{
         $this->destino_id=$destino_id;
     }
 
+/**
+     * Get the value of idGuias
+     */
+    public function getIdGuias()
+    {
+        return $this->idGuias;
+    }
 
+    /**
+     * Set the value of idGuias
+     */
+    public function setIdGuias($idGuias)
+    {
+        $this->idGuias = $idGuias;
+
+       
+    }
+
+    /**
+     * Get the value of titulo
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        
+    }
+
+    /**
+     * Get the value of comentario
+     */
+    public function getComentario()
+    {
+        return $this->comentario;
+    }
+
+    /**
+     * Set the value of comentario
+     */
+    public function setComentario($comentario)
+    {
+        $this->comentario = $comentario;
+
+        
+    }
+
+    /**
+     * Get the value of usuario_id
+     */
+    public function getUsuarioId()
+    {
+        return $this->usuario_id;
+    }
+
+    /**
+     * Set the value of usuario_id
+     */
+    public function setUsuarioId($usuario_id)
+    {
+        $this->usuario_id = $usuario_id;
+
+       
+    }
+
+    /**
+     * Get the value of destino_id
+     */
+    public function getDestinoId()
+    {
+        return $this->destino_id;
+    }
+
+    /**
+     * Set the value of destino_id
+     */
+    public function setDestinoId($destino_id)
+    {
+        $this->destino_id = $destino_id;
+
+        
+    }
 }
