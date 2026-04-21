9cdbdcbc666bb6fb82a71ffaa899cd2d1eee1883
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-21 17:43:30 +0200

    Modificando archivo autoload
    para que le la carpeta de controllers ya que estaba en nonbrada como controller

--- Proyecto/autoload.php
+++ Proyecto/autoload.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($class) {
-    $paths = ['models/', 'controller/'];
+    $paths = ['models/', 'controllers/'];
     foreach ($paths as $path) {
         $file = __DIR__ . '/' . $path . $class .  '.php';
         if (file_exists($file)) {
