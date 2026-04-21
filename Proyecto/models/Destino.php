650ce57881a3754c6c0a8bb5191e422cd6201473
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-21 18:12:25 +0200

    Añadiendo Getters  y Setters al archivo Destino.php

--- Proyecto/models/Destino.php
+++ Proyecto/models/Destino.php
@@ -21,4 +21,130 @@ class Destinos{
         $this->numero_visitas=$numero_visitas;
     }
     
+
+    /**
+     * Get the value of id_destino
+     */
+    public function getIdDestino()
+    {
+        return $this->id_destino;
+    }
+
+    /**
+     * Set the value of id_destino
+     */
+    public function setIdDestino($id_destino)
+    {
+        $this->id_destino = $id_destino;
+
+        
+    }
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+       
+    }
+
+    /**
+     * Get the value of pais
+     */
+    public function getPais()
+    {
+        return $this->pais;
+    }
+
+    /**
+     * Set the value of pais
+     */
+    public function setPais($pais)
+    {
+        $this->pais = $pais;
+
+        
+    }
+
+    /**
+     * Get the value of continente
+     */
+    public function getContinente()
+    {
+        return $this->continente;
+    }
+
+    /**
+     * Set the value of continente
+     */
+    public function setContinente($continente)
+    {
+        $this->continente = $continente;
+
+        
+    }
+
+    /**
+     * Get the value of descripcion
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set the value of descripcion
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+      
+    }
+
+    /**
+     * Get the value of imagen
+     */
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    /**
+     * Set the value of imagen
+     */
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+
+        
+    }
+
+    /**
+     * Get the value of numero_visitas
+     */
+    public function getNumeroVisitas()
+    {
+        return $this->numero_visitas;
+    }
+
+    /**
+     * Set the value of numero_visitas
+     */
+    public function setNumeroVisitas($numero_visitas)
+    {
+        $this->numero_visitas = $numero_visitas;
+
+        
+    }
 }
